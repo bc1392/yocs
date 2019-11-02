d2ed7c6b14c358ad72d4a5b23e33a9fac90660a6
Ben S <bencraft1392@gmail.com>
2019-11-02 17:07:12 -0400

    Added basis for time, fixed issues with settings menu.

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
+const setMenu = $('#set-menu-wrap')
+const setMenuGears = $('#set-menu-open, #set-menu-close')
+const setMenuOpen = $('#set-menu-open')
+const mainContain = $('#main')
+
 $(document).ready(function(){
 
-    const setMenuCtl = $('#set-menu-open, #set-menu-close')
-    $('#settings').sidenav({
-        onOpenStart: () => { setMenuCtl.addClass('screw-in') },
-        onCloseStart: () => { setMenuCtl.addClass('screw-out'); setMenuCtl.removeClass('screwed') }
+    setMenu.sidenav({
+
+        onOpenStart: () => {
+            setMenuGears.addClass('screw-in'); setMenuOpen.addClass('faded-in')
+            mainContain.addClass('blur-in')
+        },
+
+        onCloseStart: () => {
+            setMenuGears.addClass('screw-out'); setMenuGears.removeClass('screwed')
+            mainContain.addClass('blur-out'); mainContain.removeClass('blurred')
+        },
+
+        onCloseEnd: () => {
+            setMenuOpen.removeClass('faded-in')
+        }
+
     })
-    setMenuCtl.on('animationend', () => {
-        if ( setMenuCtl.hasClass('screw-in') ) { setMenuCtl.addClass('screwed'); setMenuCtl.removeClass('screw-in') }
-        if ( setMenuCtl.hasClass('screw-out') ) { setMenuCtl.removeClass('screwed screw-out') }
+
+    setMenuGears.on('animationend', () => {
+        if ( setMenuGears.hasClass('screw-in') ) { setMenuGears.addClass('screwed'); setMenuGears.removeClass('screw-in') }
+        if ( setMenuGears.hasClass('screw-out') ) { setMenuGears.removeClass('screwed screw-out') }
     })
+
+    mainContain.on('animationend', () => {
+        if ( mainContain.hasClass('blur-in') ) { mainContain.addClass('blurred'); mainContain.removeClass('blur-in') }
+        if ( mainContain.hasClass('blur-out') ) { mainContain.removeClass('blurred blur-out') }
+    })
+
+    setTimeout(() => {
+        setMenuOpen.removeClass('faded-in')
+    }, 500)
+
+    setInterval(() => {
+        const date = new Date(); const hour = date.getHours(); const minute = date.getMinutes()
+    }, 1250)
 
 })
